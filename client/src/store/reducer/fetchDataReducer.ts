dea2d4fb4aff90a1ea7dd1d4070dd6cc5cdaf7e4
phunguyen95 <phu.nguyen09995@gmail.com>
2021-10-26 20:01:34 +0700

    fix lint

--- client/src/store/reducer/fetchDataReducer.ts
+++ client/src/store/reducer/fetchDataReducer.ts
@@ -1,15 +1,12 @@
-/* Structure of the taskify state store
+/* Structure of the application state store
   state : {
-
-    myBoards : Boolean,
-    boards :{
-        bordId : Boolean
+    myRepos : Boolean,
+    repos :{
+        repoId : Boolean
     }
   }
    */
-
   import {
-    FETCH_INDIVIDUAL_REPO,
     FETCH_REPOS,
     SET_INITIAL_STATE_FETCH_REDUCER,
   } from '../types';
@@ -20,18 +17,12 @@
   }
   
   const fetchReducer = (state = initState, action:any) => {
-    console.log('action',action.type)
     switch (action.type) {
       case FETCH_REPOS:
         return {
           ...state,
           myRepos: true,
         }
-    //   case FETCH_INDIVIDUAL_REPO:
-    //     state.repos[action.payload] = true
-    //     return {
-    //       ...state,
-    //     }
       case SET_INITIAL_STATE_FETCH_REDUCER:
         return {}
       default:
