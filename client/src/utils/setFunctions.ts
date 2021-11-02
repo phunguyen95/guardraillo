4a17d10aec6da49f4588ba1f4d05ba620ca670ff
phunguyen95 <phu.nguyen09995@gmail.com>
2021-11-02 17:45:13 +0700

    final version

--- client/src/utils/setFunctions.ts
+++ client/src/utils/setFunctions.ts
@@ -10,16 +10,9 @@ import {
     dispatch:any,
     callBack = () => {},
   ) => {
-    const {statusCode, data} = await ApiAction.getRequest('/repo')
+    const {data} = await ApiAction.getRequest('/repo')
 
-    // if (statusCode === 400 || statusCode === 500) {
-    //   // toast.error(data)
-    //   logout() (for login future implementation)
-    //   replace('/signin')
-    //   return
-    // }
     const res = JSON.parse(data)
-    console.log({res: res})
     setRepos(dispatch, res.repos)
   
     fetchMyRepos(dispatch)
@@ -36,9 +29,8 @@ import {
     const url = '/repo/' + repoId 
     if (isMyRepoFetch) await setReposInStore(dispatch)
   
-    const {statusCode, data} = await ApiAction.getRequest(url)
+    const {data} = await ApiAction.getRequest(url)
     const res = JSON.parse(data)
-    console.log({res})
     setColumns(dispatch, {repoId, columns: res.columns})
     fetchIndividualRepo(dispatch, repoId)
     callBack()
